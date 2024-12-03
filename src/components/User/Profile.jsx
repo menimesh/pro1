89be822419f9bf0ebc8d7a0771a8733db28e888a
menimesh <nimeshkhatiwadaa@gmail.com>
2024-12-03 15:53:50 +0545

    backend

--- src/components/User/Profile.jsx
+++ src/components/User/Profile.jsx
@@ -1,73 +1,101 @@
 import React, { useEffect, useState } from 'react';
 import Layout from './Layout';
-import avatar from '../img/customer.jpg'
-import { onAuthStateChanged,getAuth } from 'firebase/auth';
+import avatar from '../img/customer.jpg';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import firebaseAppConfig from '../../util/firebase-config';
-const auth=getAuth(firebaseAppConfig)
+import { getFirestore,collection,addDoc,where,getDocs,query } from 'firebase/firestore';
+import Swal from 'sweetalert2';
+const auth = getAuth(firebaseAppConfig);
+const db=getFirestore(firebaseAppConfig);
+
+
 const Profile = () => {
-    const navigate=useNavigate();
- 
-    
-    const [session,setSession]=useState(null)
-    const [formValue,setFormValue]=useState({
-      fullname:'',
-      email:'',
-      mobile:'',
-      area:'',
-      city:'',
-      country:'',
-      code:''
-    })
-    
-    useEffect(()=>{
-onAuthStateChanged(auth,(user)=>{
-    if(user){
-navigate('/Profile')
-setSession(user)
-setFormValue({
-  fullname: user.displayName || '',
+  const navigate = useNavigate();
+  
+  const [session, setSession] = useState(null);
+  const [formValue, setFormValue] = useState({
+    fullname: '',
+    email: '',
+    mobile: '',
+  });
+
+  const [addressValue, setAddressValue] = useState({
+    area: '',
+    city: '',
+    country: '',
+    code: '',
+    userID:''
+  });
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('/Profile');
+        setSession(user);
+        setFormValue({
+          fullname: user.displayName || '',
           email: user.email || '',
           mobile: '',
-          area: '',
-          city: '',
-          country: '',
-          code: '',
+        });
+        setAddressValue({
+          ...addressValue,
+          userID: user.uid
+        })
+       const ref=collection(db,"addresses")
+       query(ref,where("userID","==",user.uid))
+      } else {
+        setSession(false);
+        navigate('/login');
+      }
+    });
+  }, []);
 
-})
+  const handleProfileFormValue = (e) => {
+    const input = e.target;
+    const name = input.name;
+    const value = input.value;
 
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  };
 
-    }else{
-setSession(false)
-navigate('/login')
-setSession(false)
+  const handleAddressFormValue = (e) => {
+    const input = e.target;
+    const name = input.name;
+    const value = input.value;
+
+    setAddressValue({
+      ...addressValue,
+      [name]: value,
+    });
+  };
+
+  const saveAddress = async(e) => {
+    try{
+      e.preventDefault();
+      
+      await addDoc(collection(db,"addresses"),addressValue)
+     new Swal({
+      icon:'sucess',
+      title: 'Address Saved!'
+
+     })
+      
+    }catch(err){
+       new Swal({
+        icon:'error',
+        title: 'Failed',
+        text: err.message
+       })
     }
-})
-    },[])
-    const handleChange =(e)=>{
-      const input = e.target
-      const value=input.value
-      const name=input.name
-      setFormValue({
-        ...formValue,
-        [name]:value
-      })
+    // Save address to the database or handle accordingly
+  };
 
-    }
-
-    // if (session===null) {
-    //     return (
-    //       <div className="bg-gray-100 h-screen fixed top-0 left-0 w-full flex justify-center items-center">
-    //         <span className="relative flex h-3 w-3">
-    //           <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-    //           <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-    //         </span>
-    //       </div>
-    //     );
-    //   }
   return (
     <Layout>
-      
       <div className='mx-auto md:my-16 shadow-lg rounded-md p-8 md:w-8/12 lg:w-6/12 border'>
         <div className='flex gap-3 items-center'>
           <i className='ri-user-fill text-4xl'></i>
@@ -75,8 +103,7 @@ setSession(false)
         </div>
         <hr className='my-6' />
         <div>
-        <img src={avatar} alt="" className='rounded h-[150px] w-[150px] mx-auto'/>
-       
+          <img src={avatar} alt="" className='rounded h-[150px] w-[150px] mx-auto'/>
         </div>
         <form className='grid grid-cols-2 sm:grid-cols-2 gap-6'>
           <div className='flex flex-col gap-2'>
@@ -84,13 +111,12 @@ setSession(false)
               Full name
             </label>
             <input
-              onChange={handleChange}
               type='text'
               name='fullname'
               id='fullname'
               className='p-3 rounded border border-gray-300'
               value={formValue.fullname}
-           
+              onChange={handleProfileFormValue}
             />
           </div>
           <div className='flex flex-col gap-2'>
@@ -98,13 +124,12 @@ setSession(false)
               Email
             </label>
             <input
-            onChange={handleChange}
               type='email'
               name='email'
               id='email'
               className='p-3 rounded border border-gray-300'
               value={formValue.email}
-            
+              onChange={handleProfileFormValue}
             />
           </div>
           <div className='flex flex-col gap-2'>
@@ -112,27 +137,34 @@ setSession(false)
               Mobile
             </label>
             <input
-            onChange={handleChange}
               type='number'
               name='mobile'
               id='mobile'
               className='p-3 rounded border border-gray-300'
               value={formValue.mobile}
+              onChange={handleProfileFormValue}
             />
           </div>
-         
-          <div></div>
+          
+        </form>
+
+        <hr className='my-6' />
+        
+        <div className='mx-auto rounded h-[50px] w-[170px] text-xl font-bold text-gray-600'>
+          Delivery address</div>
+
+        <form className='grid grid-cols-2 gap-6' onSubmit={saveAddress}>
           <div className='flex flex-col gap-2'>
             <label className='text-lg font-semibold' htmlFor='area'>
               Area
             </label>
             <input
-            onChange={handleChange}
+              onChange={handleAddressFormValue}
               type='text'
               name='area'
               id='area'
               className='p-3 rounded border border-gray-300'
-              value={formValue.area}
+              value={addressValue.area}
             />
           </div>
           <div className='flex flex-col gap-2'>
@@ -140,44 +172,45 @@ setSession(false)
               City
             </label>
             <input
-            onChange={handleChange}
+              onChange={handleAddressFormValue}
               type='text'
               name='city'
-              id=''
+              id='city'
               className='p-3 rounded border border-gray-300'
-              value={formValue.city}
+              value={addressValue.city}
             />
           </div>
           <div className='flex flex-col gap-2'>
-            <label className='text-lg font-semibold' htmlFor='city'>
+            <label className='text-lg font-semibold' htmlFor='country'>
               Country
             </label>
             <input
-            onChange={handleChange}
+              onChange={handleAddressFormValue}
               type='text'
               name='country'
               id='country'
               className='p-3 rounded border border-gray-300'
-              value={formValue.country}
+              value={addressValue.country}
             />
           </div>
           <div className='flex flex-col gap-2'>
-            <label className='text-lg font-semibold' htmlFor='city'>
+            <label className='text-lg font-semibold' htmlFor='code'>
               Pincode
             </label>
             <input
-            onChange={handleChange}
+              onChange={handleAddressFormValue}
               type='text'
               name='code'
               id='code'
               className='p-3 rounded border border-gray-300'
-              value={formValue.code}
+              value={addressValue.code}
             />
           </div>
-          <button className='px-4 py-2 bg-rose-600 text-white rounded w-fit hover:bg-green-600 '>
-          <i class="ri-save-line mr-2"></i>
-            Save</button>
+          <button type="submit" className='px-4 py-2 bg-rose-600 text-white rounded w-fit hover:bg-green-600'>
+            <i className="ri-save-line mr-2"></i> Save
+          </button>
         </form>
+        
       </div>
     </Layout>
   );
